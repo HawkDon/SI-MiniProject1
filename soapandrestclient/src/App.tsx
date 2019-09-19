bde6e19accaff407969742f195d019fa1c990e7f
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-09-19 13:50:25 +0200

    initials

--- soapandrestclient/src/App.tsx
+++ soapandrestclient/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  React.useEffect(() => {
+    const resizeListener = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", resizeListener);
+
+    return () => window.removeEventListener("resize", resizeListener);
+  }, [windowWidth]);
+  return <div style={{ width: windowWidth }}>{windowWidth}</div>;
+};
 
 export default App;
